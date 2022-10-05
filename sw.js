4ed6196e2af94b748a3151a98932c9be2032a777
Vanne Vega <manelp3214@gmail.com>
2022-10-05 13:25:52 -0500

    Formas validas para hacer peticiones

--- sw.js
+++ sw.js
@@ -1,3 +1,13 @@
 
 
-console.log('SW: Hola mundo');
+
+self.addEventListener('fetch', event => {
+    
+    if(event.request.url.includes('.jpg')) {
+        console.log(event.request.url);
+
+        let fotoReq = fetch(event.request);
+        event.respondWith(fotoReq);
+    }
+ 
+});
